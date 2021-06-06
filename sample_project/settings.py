5e16612c1db283215d2c6ceaf0732ff6b1f2d4c4
RyosukeSomeya <rx.78.0618@gmail.com>
2021-06-07 02:44:24 +0900

    modify settings.py:8

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -130,7 +130,7 @@ LOGIN_REDIRECT_URL = 'myapp:index'
 
 #S3セッティング
 AWS_STORAGE_BUCKET_NAME = "dev-django-sample01"
-AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = "%s.s3-ap-northeast-1.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
