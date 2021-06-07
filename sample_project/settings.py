b0ed9658838166d1b18e5718c16e6e8a313162e4
RyosukeSomeya <rx.78.0618@gmail.com>
2021-06-07 16:20:47 +0900

    edit settings

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -146,6 +146,7 @@ ALLOWED_HOSTS = ['*']
 
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEBUG = False
 
