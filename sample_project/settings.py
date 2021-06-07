f9be409b1e3975d9b7950f1922922d6d9c654cac
RyosukeSomeya <rx.78.0618@gmail.com>
2021-06-07 15:04:10 +0900

    modify path settings:2

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -144,10 +144,10 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FOWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 DEBUG = False
 
