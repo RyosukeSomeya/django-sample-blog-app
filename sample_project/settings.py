d1aa7bbf1fbdeecdd8e3785fde594ed340b56794
RyosukeSomeya <rx.78.0618@gmail.com>
2021-06-08 00:56:22 +0900

    debug off

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -143,6 +143,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FOWARDED_PROTO', 'https')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
