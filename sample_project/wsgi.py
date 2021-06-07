4e980118a1cb03d89190cbebacd70190230ae0f1
RyosukeSomeya <rx.78.0618@gmail.com>
2021-06-07 15:19:43 +0900

    modify path settings:5

--- sample_project/wsgi.py
+++ sample_project/wsgi.py
@@ -2,8 +2,11 @@ import os
 
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
+from whitenoise.Django import DjangoWhiteNoise
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sample_project.settings")
 
 application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
 # application = get_wsgi_application()
