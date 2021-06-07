fbee51ce2f770f0c24da200e479855ba0ed6c2a8
RyosukeSomeya <rx.78.0618@gmail.com>
2021-06-08 01:13:16 +0900

    debug on

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -158,7 +158,7 @@ try:
 except ImportError:
     pass
 
-if not DEBUG:
+if DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
