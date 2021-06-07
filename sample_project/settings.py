56b7bb7ed36c7c6d7d100fa7a17b3705db3347b5
RyosukeSomeya <rx.78.0618@gmail.com>
2021-06-07 16:33:12 +0900

    edit setting.py

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -145,7 +145,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FOWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 DEBUG = False
 
@@ -166,6 +166,4 @@ if not DEBUG:
     }
 
     AWS_LOCATION = 'media'
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
